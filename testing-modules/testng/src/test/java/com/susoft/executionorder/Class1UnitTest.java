29450074281d2587bead136ad6711344b502072d
Suresh Jaganathan <sureshnath@hotmail.com>
2020-04-26 00:49:14 +0100

    on multiple forks

---
+++ testing-modules/testng/src/test/java/com/susoft/executionorder/Class1UnitTest.java
@@ -0,0 +1,11 @@
+package com.susoft.executionorder;
+
+import com.susoft.AbstractTestBase;
+import org.testng.annotations.Test;
+
+public class Class1UnitTest extends AbstractTestBase {
+    @Test
+    public void testClass1method1(){
+        LOGGER.entry();
+    }
+}
---
+++ testing-modules/testng/src/test/java/com/susoft/executionorder/Class2UnitTest.java
@@ -0,0 +1,11 @@
+package com.susoft.executionorder;
+
+import com.susoft.AbstractTestBase;
+import org.testng.annotations.Test;
+
+public class Class2UnitTest extends AbstractTestBase {
+    @Test
+    public void testClass2method1(){
+        LOGGER.entry();
+    }
+}
